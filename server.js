275c6599e192e3d69314f6b64db391cc082d9181
Glitch (suave-plume-cross) <none>
2024-03-09 04:27:28 +0000

    🔨🚙 Checkpoint ./img/main.js:58069725/9 ./main.js:58069725/3001 ./img/index.html:58069725/5 ./server.js:58069725/3480 ./public/index.html:58069725/10057 ./package.json:58069725/193

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 import express from 'express';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
+const port = process.env.PORT || 3000;
 
-// Serve static files from the 'dist' directory (or 'public', depending on your build config)
-app.use(express.static(path.join(__dirname, 'img')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Send your main HTML file in response to get requests on the root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'img', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
